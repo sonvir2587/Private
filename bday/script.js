6c36edf909b50bb82d2e4be20bd8d79712093417
sonvir2587 <sonvir2587@gmail.com>
2025-05-25 20:16:00 +0530

    script.js

--- bday/script.js
+++ bday/script.js
@@ -1,1 +1,12 @@
+const flowersContainer = document.querySelector('.flowers');
 
+function createFlower() {
+  const flower = document.createElement('div');
+  flower.classList.add('flower');
+  flower.style.left = Math.random() * 100 + 'vw';
+  flower.style.animationDuration = 5 + Math.random() * 5 + 's';
+  flowersContainer.appendChild(flower);
+  setTimeout(() => flower.remove(), 10000);
+}
+
+setInterval(createFlower, 300);
